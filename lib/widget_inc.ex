54c5d38adbdd98f76b7524209667e5a7baa2c064
Nicholas Henry <nicholas.henry@gmail.com>
2017-06-03 07:54:04 -0400

    Generate application

---
+++ lib/widget_inc.ex
@@ -0,0 +1,18 @@
+defmodule WidgetInc do
+  @moduledoc """
+  Documentation for WidgetInc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WidgetInc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/widget_inc_test.exs
@@ -0,0 +1,8 @@
+defmodule WidgetIncTest do
+  use ExUnit.Case
+  doctest WidgetInc
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
